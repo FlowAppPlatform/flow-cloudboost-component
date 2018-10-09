d2d794f977dc1957d43dc2c205ad4df32cd7facb
Jerry Edebua <jeredebua@gmail.com>
2018-10-09 15:12:56 +0300

    removed general component class

--- src/query.js
+++ src/query.js
@@ -1,13 +1,21 @@
 var Flow = require('flow-platform-sdk');
-var Component = require('./component');
 var API = require('./api');
 
-class QueryComponent extends Component {
+class QueryComponent extends Flow.Component {
 
   constructor() {
 
     super();    
     this.name = 'Query Component';
+
+    var app_id = new Flow.Property('APP_ID', 'text');
+    app_id.required = true;
+
+    var client_key = new Flow.Property('CLIENT_KEY', 'text');
+    client_key.required = true;
+
+    var table = new Flow.Property('Table', 'text');
+    table.required = true;
 
     /* 
     * 'Constraints' sample structure 
@@ -18,7 +26,23 @@ class QueryComponent extends Component {
     * 
     */
     var constraints = new Flow.Property('Constraints', 'object');
+    
+    this.addProperty(app_id);
+    this.addProperty(client_key);
+    this.addProperty(table);
     this.addProperty(constraints);
+
+    var success = new Flow.Port('Success');
+    var error = new Flow.Port('Error');
+    
+    var response = new Flow.Property('Data', 'object');
+    success.addProperty(response);
+
+    var generalError = new Flow.Property('Data', 'object');
+    error.addProperty(generalError);
+
+    this.addPort(success);
+    this.addPort(error);
 
     // query with constraints here
     this.attachTask(function () {
